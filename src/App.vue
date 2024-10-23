412454759a3e92f62fcb87481cec5f7bf9a83b20
lengkimtry <kimt47494@gmail.com>
2024-10-23 08:36:15 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+<h1>This is my first VueJS Project.</h1>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+
+</style>
